866a77422fd4e40b795c6477cc975b771cc9f718
rexDainiel <rexdaniel35@gmail.com>
2014-12-13 23:23:06 +0800

    several change

--- js/Likins.js
+++ js/Likins.js
@@ -1,33 +1,40 @@
 $(function() {
-	 var winH = $(window).height();
-	 var winW = $(window).width();
-	 $(".bg-can").height(winH);
-	 $(".submit").css({"margin-top":winH/2-177+"px","margin-left":winW/2-195+"px"})
+	var winH = $(window).height();
+	var winW = $(window).width();
+	$(".bg-can").height(winH);
+	$(".submit").css({"margin-top":winH/2-177+"px","margin-left":winW/2-195+"px"})
 
 
 
-	 // change sign in and sign up
-	 $("#change-signup").click(function(){
+	// change sign in and sign up
+	$("#change-signup").click(function(){
 	 	$("input[type=submit]").attr("value","登    陆");
 	 	$("#change-signup").attr("class","hidden");
 	 	$("#change-signin").attr("class","");
-	 });	 
-	 $("#change-signin").click(function(){
+	});	 
+	$("#change-signin").click(function(){
 	 	$("input[type=submit]").attr("value","加    入");
 	 	$("#change-signin").attr("class","hidden");
 	 	$("#change-signup").attr("class","");
-	 });
+	});
 
-	 // background slide up
-	 $(".bg-can img").animate({
+	// background slide up
+	$(".bg-can img").animate({
 	 	top: "-100px"
-	 },5000)
+	},5000)
 
 
+	$("#home-main .photo-collapse>img").click(function(){
+	 	$("#bg-shadow").attr("class","");
+	 	$("#img-detail").attr("class","");
+		$("#img-detail").css("left",winW/2-400+"px")
+		photoOn();
+	});
+	$("#bg-shadow").click(function(){
+	 	$("#bg-shadow").attr("class","hidden");
+	 	$("#img-detail").attr("class","hidden");
+	});
 
-
-	 $("#img-detail").css("left",winW/2-400+"px")
-	 photoOn();
 })
 
 
